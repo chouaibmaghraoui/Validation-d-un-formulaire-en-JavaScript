66d4d55aff3f2a35a3bf33863c46247cf022e565
mon cv <80108568+chouaibmaghraoui@users.noreply.github.com>
2021-06-22 09:17:38 +0100

    Add files via upload

---
+++ Validation d'un formulaire en JavaScript/index.js
@@ -0,0 +1,16 @@
+function verif(){
+    if(f.np.value =="")
+    {
+        alert("champ nom et prénom est obligatoire");
+        return false;
+        elsei if (f.m.value){
+            alert("champ mailest  obligatoire");
+            return false}
+            else if ( (f.m.value.indexof('0')==-1)//
+            (f.m.verif.indexof('.')<0) )
+            alert("vérifier que votre mail est correct");
+            return false
+
+        }
+    }
+}     
